980772046121db6ff6ec28b1fbc5bb0b96d92d3b
Javier Silva <javsilsol23@gmail.com>
2023-06-10 12:15:37 +0200

    add explanation of the cycles

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,6 @@ def load_data():
 st.set_page_config(layout="wide")
 st.title('Nick Dagan Best Venus Cycle Calculator')
 
-data_load_state = st.text('Loading data...')
 data = load_data()
 
 d = st.date_input(
@@ -65,3 +64,32 @@ else:
 st.markdown(
     f'<div style="background-color="#f0f0f0"><p style = "font-size: 18px; font-weight: bold;color:{interval_fg_color}; background-color:{interval_bg_color};"> Interval {interval_color.upper()} {interval_number}</p><p style = "font-size: 18px; font-weight: bold;color:{phase_fg_color}; background-color:{phase_bg_color};"> Phase {phase_color} {phase_letter.upper()}</p></div>', unsafe_allow_html=True
 )
+
+st.markdown('''
+The venus synodic cycle consists of 2920 days (8 years minus 2 days).
+
+It can be divided into 20 parts averaging 146 days each.
+
+- White Intervals
+  - 141 - 151 days
+  - Morning Star
+  - Gradual Acceleration
+- Blue Intervals
+  - 146 days
+  - Rising to Setting Phase
+  - Fast Direct Motion
+  - Exterior Conjunction at the Center of Interval
+- Black Intervals
+  - 141 - 151 days
+  - Evening Star
+  - Gradual Deceleration
+- Red Intervals
+  - 146 days
+  - Setting to Rising Phase
+  - Slow Retrograde Motion
+  - Interior Conjunction at the Center of Interval
+
+It can also be divided into 10 equal parts. Split this parts into alternating black and white sections.
+
+For more information, check out [this video](https://www.youtube.com/watch?v=JF8cvLfyOo8).
+''')
